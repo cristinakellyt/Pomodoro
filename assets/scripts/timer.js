81a142611dbe6edad8b13091862dcf61c8a8e2ea
Cristina Kelly <cristinakellyt@gmail.com>
2022-12-08 16:56:49 +0100

    Set partially timer to countdown

--- assets/scripts/timer.js
+++ assets/scripts/timer.js
@@ -2,19 +2,30 @@ const focusTimer = document.getElementById('focus-timer');
 const shortBreak = document.getElementById('short-break');
 const longBreak = document.getElementById('long-break');
 const allTimerTypes = document.querySelectorAll('.timer-type h2');
+const allTimerType = document.querySelector('.timer-type');
+const countdown = document.querySelector('.countdown');
+const btnStart = document.getElementById('btn-start');
+let minutes = 25;
+let seconds = 0;
 
-function changeColorHandler(color, allTimerTypes, time, event) {
+function setCountdown(min, sec) {
+  minutes = min < 10 ? '0' + min : min;
+  seconds = sec < 10 ? '0' + sec : sec;
+  countdown.textContent = `${minutes}:${seconds}`;
+}
+
+setCountdown(minutes, seconds);
+
+function changeColorHandler(color, allTimerTypes, min, sec, event) {
   const main = document.getElementById('main');
   const sectionTimer = document.getElementById('section-timer');
-  const btnStart = document.getElementById('btn');
-  const countdown = document.querySelector('.countdown');
 
   main.style.backgroundColor = `var(--${color}-light)`;
   sectionTimer.style.backgroundColor = `var(--${color}-accent-light)`;
   btnStart.style.backgroundColor = `var(--${color}-accent)`;
-  countdown.textContent = time;
   event.target.style.backgroundColor = `var(--${color}-primary)`;
-  console.log(event);
+
+  setCountdown(min, sec);
 
   allTimerTypes.forEach((element) => {
     element.addEventListener(
@@ -29,17 +40,39 @@ function changeColorHandler(color, allTimerTypes, time, event) {
   });
 }
 
+function startTimer() {
+  minutes = Number(minutes);
+  seconds = Number(seconds);
+
+  let timerTotal = minutes * 60 + seconds;
+
+  let timer = setInterval(() => {
+    timerTotal--;
+    if (timerTotal <= 0) {
+      clearInterval(timer);
+    }
+    minutes = parseInt(timerTotal / 60);
+    seconds = parseInt(timerTotal % 60);
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdown.textContent = minutes + ':' + seconds;
+  }, 1000);
+}
+
 shortBreak.addEventListener(
   'click',
-  changeColorHandler.bind(this, 'teal', allTimerTypes, '5:00')
+  changeColorHandler.bind(this, 'teal', allTimerTypes, 0, 10)
 );
 
 longBreak.addEventListener(
   'click',
-  changeColorHandler.bind(this, 'indigo', allTimerTypes, '15:00')
+  changeColorHandler.bind(this, 'indigo', allTimerTypes, 15, 00)
 );
 
 focusTimer.addEventListener(
   'click',
-  changeColorHandler.bind(this, 'red', allTimerTypes, '25:00')
+  changeColorHandler.bind(this, 'red', allTimerTypes, 25, 00)
 );
+btnStart.addEventListener('click', startTimer);
