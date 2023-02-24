290fef6622cb6ada8a9d7eb8cb99bdd976f64ce1
Cristina Kelly <cristinakellyt@gmail.com>
2023-02-24 11:21:34 +0100

    Refactor Modal file Move the code in createElements method to the constructor and add default values when destructuring the object received. Add validation to check if the paramenters received in onConfirm(), onCancel(), and appendTo() are correct. Modify the methods that change the colors to setters.

--- src/modal.js
+++ src/modal.js
@@ -1,66 +1,75 @@
 class Modal {
   #mainDiv;
   #contentBox;
-  #titleEl;
-  #subtitleEl;
-  #buttonsDiv;
   #confirmBtnEl;
   #cancelBtnEl;
   #cancelCallback;
   #confirmCallback;
 
-  constructor(modalContent) {
-    this.#createElements(modalContent);
-  }
-
-  #createElements(modalContent) {
+  constructor({
+    title = 'Title',
+    subtitle = 'Subtitle',
+    colorDark = 'black',
+    colorLight = 'pink',
+    confirmBtnText = 'Yes',
+    cancelBtnText = 'No',
+  }) {
     this.#mainDiv = document.createElement('div');
     this.#mainDiv.className = 'modal';
 
     this.#contentBox = document.createElement('div');
     this.#contentBox.className = 'modal-box';
-    this.#contentBox.style.borderColor = `${modalContent.colorDark}`;
+    this.#contentBox.style.borderColor = colorDark;
 
-    this.#titleEl = document.createElement('h2');
-    this.#titleEl.textContent = modalContent.title;
+    const titleEl = document.createElement('h2');
+    titleEl.textContent = title;
 
-    this.#subtitleEl = document.createElement('p');
-    this.#subtitleEl.textContent = modalContent.subtitle;
-
-    this.#buttonsDiv = document.createElement('div');
-    this.#buttonsDiv.className = 'modal-box__btns';
+    const subtitleEl = document.createElement('p');
+    subtitleEl.textContent = subtitle;
 
     this.#confirmBtnEl = document.createElement('button');
     this.#confirmBtnEl.className = 'btn';
-    this.#confirmBtnEl.textContent = modalContent.confirmBtnText;
-    this.#confirmBtnEl.style.backgroundColor = modalContent.colorLight;
+    this.#confirmBtnEl.textContent = confirmBtnText;
+    this.#confirmBtnEl.style.backgroundColor = colorLight;
 
     this.#cancelBtnEl = document.createElement('button');
     this.#cancelBtnEl.className = 'btn';
-    this.#cancelBtnEl.textContent = modalContent.cancelBtnText;
-    this.#cancelBtnEl.style.backgroundColor = modalContent.colorDark;
+    this.#cancelBtnEl.textContent = cancelBtnText;
+    this.#cancelBtnEl.style.backgroundColor = colorDark;
 
-    this.#contentBox.appendChild(this.#titleEl);
-    this.#contentBox.appendChild(this.#subtitleEl);
-    this.#contentBox.appendChild(this.#buttonsDiv);
-    this.#buttonsDiv.appendChild(this.#cancelBtnEl);
-    this.#buttonsDiv.appendChild(this.#confirmBtnEl);
+    const buttonsDiv = document.createElement('div');
+    buttonsDiv.className = 'modal-box__btns';
+    buttonsDiv.appendChild(this.#cancelBtnEl);
+    buttonsDiv.appendChild(this.#confirmBtnEl);
+
+    this.#contentBox.appendChild(titleEl);
+    this.#contentBox.appendChild(subtitleEl);
+    this.#contentBox.appendChild(buttonsDiv);
     this.#mainDiv.appendChild(this.#contentBox);
   }
 
   onConfirm(callback) {
+    if (typeof callback !== 'function')
+      throw new TypeError('callback is not a function');
+
     this.#confirmBtnEl.removeEventListener('click', this.#confirmCallback);
     this.#confirmCallback = callback;
     this.#confirmBtnEl.addEventListener('click', this.#confirmCallback);
   }
 
   onCancel(callback) {
+    if (typeof callback !== 'function')
+      throw new TypeError('callback is not a function');
+
     this.#cancelBtnEl.removeEventListener('click', this.#cancelCallback);
     this.#cancelCallback = callback;
     this.#cancelBtnEl.addEventListener('click', this.#cancelCallback);
   }
 
   appendTo(element) {
+    if (!(element instanceof HTMLElement))
+      throw new TypeError('element is not an instance of HTMLElement');
+
     element.appendChild(this.#mainDiv);
   }
 
@@ -72,15 +81,15 @@ class Modal {
     this.#mainDiv.classList.remove('show-modal');
   }
 
-  borderColor(color) {
+  set borderColor(color) {
     this.#contentBox.style.borderColor = color;
   }
 
-  confirmBtnBackgroundColor(color) {
+  set confirmBtnColor(color) {
     this.#confirmBtnEl.style.backgroundColor = color;
   }
 
-  cancelBtnBackgroundColor(color) {
+  set cancelBtnColor(color) {
     this.#cancelBtnEl.style.backgroundColor = color;
   }
 }
